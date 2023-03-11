bd289c7989eeaa3616a5e9cb0fa65d211c9edf4b
negin-titanium <neginshahany@gmail.com>
2023-03-12 00:39:29 +0330

    add Js-string code from codesandbox

---
+++ ShecodesPlus- HW03/JS-strings.js
@@ -0,0 +1,16 @@
+let country = "Guinea Bissau";
+let city = " Sydney   ";
+let place = "School";
+let attraction = "Opera House";
+// challenge 1 solution
+console.log(country.split(" ").join("-"));
+// challenge 2 solution
+console.log(city.trim());
+// challenge 3 solution
+console.log(attraction.toLowerCase());
+// challenge 4 solution
+console.log(place.toUpperCase());
+// challenge 5 solution
+console.log(
+    `I went to visit the ${attraction} in ${city.trim()}, right next to my school. Now, I'm getting ready for my trip to ${country}!`
+);
