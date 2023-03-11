ecf79d8dbf95fa9fbb1e07d203bc78a129d1742d
negin-titanium <neginshahany@gmail.com>
2023-03-12 00:50:31 +0330

    add Js-math code from codesandbox

---
+++ ShecodesPlus- HW03/JS-MAth.js
@@ -0,0 +1,11 @@
+let price = 4.5;
+// challenge 1 solution
+console.log(Math.round(price));
+// challenge 2 solution
+console.log(Math.ceil(price));
+// challenge 3 solution
+console.log(Math.floor(price));
+// challenge 4 solution
+console.log(Math.min(4, 1, 3, 10));
+// challenge 5 solution
+console.log(Math.max(4, 1, 3, 10));
